588f2d945881850dcaf044aaf4612d7fc9c7116d
nakao53 <nakao53@github.com>
2022-05-26 23:57:18 +0900

    Initial commit

---
+++ src/goods/Goods.java
@@ -0,0 +1,5 @@
+package goods;/**
+ * @author Nakao Mizuki
+ * Date: 2022/05/26 
+ */public class Goods {
+}
---
+++ src/order/Cart.java
@@ -0,0 +1,5 @@
+package order;/**
+ * @author Nakao Mizuki
+ * Date: 2022/05/26 
+ */public class Cart {
+}
